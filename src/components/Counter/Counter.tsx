28bf29df3d67357e3933c9afafa3e25004c2f28a
Mix-Liten <mixliten@gmail.com>
2024-01-10 14:58:55 +0800

    feat: update <Counter /> style and use devtools

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,23 +1,37 @@
 import { useEffect } from 'react'
 import { useCounterStore } from './store'
 
+import './Counter.css'
+
 function initCount() {
   useCounterStore.setState({ count: 1 })
 }
 
 function Counter({ count }: { count: number }) {
-  const { increment, incrementAsync, decrement } = useCounterStore.getState()
+  const { increment, incrementAsync, decrement, decrementAsync } = useCounterStore.getState()
 
   useEffect(() => {
     initCount()
   }, [])
 
   return (
-    <div>
-      <p style={{ textAlign: 'center' }}>{count}</p>
-      <button onClick={increment}>+</button>
-      <button onClick={incrementAsync}>+~</button>
-      <button onClick={decrement}>-</button>
+    <div className="wrapper-root">
+      <p className="content">
+        Current count: <br />
+        {count}
+      </p>
+      <div className="wrapper">
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+      <div className="wrapper">
+        <button onClick={incrementAsync} title="delay increment">
+          (+)
+        </button>
+        <button onClick={decrementAsync} title="delay decrement">
+          (-)
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/Counter/store.ts
+++ src/components/Counter/store.ts
@@ -1,22 +1,33 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 type CounterStore = {
   count: number
   increment: () => void
   incrementAsync: () => Promise<void>
   decrement: () => void
+  decrementAsync: () => Promise<void>
 }
 
-export const useCounterStore = create<CounterStore>(set => ({
-  count: 0,
-  increment: () => {
-    set(state => ({ count: state.count + 1 }))
-  },
-  incrementAsync: async () => {
-    await new Promise(resolve => setTimeout(resolve, 1e3))
-    set(state => ({ count: state.count + 1 }))
-  },
-  decrement: () => {
-    set(state => ({ count: state.count - 1 }))
-  },
-}))
+export const useCounterStore = create<CounterStore>()(
+  devtools(
+    set => ({
+      count: 0,
+      increment: () => {
+        set(state => ({ count: state.count + 1 }))
+      },
+      incrementAsync: async () => {
+        await new Promise(resolve => setTimeout(resolve, 1e3))
+        set(state => ({ count: state.count + 1 }))
+      },
+      decrement: () => {
+        set(state => ({ count: state.count - 1 }))
+      },
+      decrementAsync: async () => {
+        await new Promise(resolve => setTimeout(resolve, 1e3))
+        set(state => ({ count: state.count - 1 }))
+      },
+    }),
+    { name: 'counterStore' },
+  ),
+)
